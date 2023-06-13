8c0a69c6602e02ff5cb6855cb7c5fa5aa87fd1cf
refrain62 <nao_h13@hotmail.com>
2023-06-14 00:32:07 +0900

    フォームの input 要素のリセット

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,8 +1,7 @@
 import prisma from '@/lib/prisma';
-import { revalidatePath } from 'next/cache';
-
 import DoneTodo from './DoneTodo';
-import { addTodo, deleteTodo, doneTodo } from './actions';
+import AddTodo from './AddTodo';
+import { deleteTodo, doneTodo } from './actions';
 
 const Page = async () => {
     const todos = await prisma.todo.findMany();
@@ -35,16 +34,7 @@ const Page = async () => {
                 ))}
             </ul>
 
-            <form className="flex items-center mt-4" action={addTodo}>
-                <label htmlFor="name">Name:</label>
-                <input type="text" name="name" className="border mx-2 p-1" />
-                <button
-                    type="submit"
-                    className="bg-blue-600 px-2 py-1 rounded-lg text-sm text-white"
-                    >
-                        Add Todo
-                    </button>
-            </form>
+            <AddTodo />
         </div>
     );
 };
