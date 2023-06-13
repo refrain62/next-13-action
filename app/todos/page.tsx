97774c48e25600e7ff8646f370f0a8b99f808d4f
refrain62 <nao_h13@hotmail.com>
2023-06-13 23:28:26 +0900

    isCompletedのときの処理追加

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -33,22 +33,20 @@ const Page = async () => {
             <h1 className="text-xl font-bold">Todo一覧</h1>
             <ul className="mt-8">
                 {todos.map((todo) => (
-                    <li key={todo.id} className="flex items-center space-x-2">
+                    <li 
+                        key={todo.id}
+                        className={`flex items-center space-x-2 ${
+                            todo.isComplated ? 'line-through' : ''
+                        }`}>
+                        <span>{todo.name}</span>
                         <form>
+                            <input type="hidden" name="id" value={todo.id} />
                             <button
                                 className="bg-red-500 px-2 py-1 rounded-lg text-sm text-white"
-                                formAction={doneTodo}
+                                formAction={deleteTodo}
                                 >
-                                    完了
+                                    削除
                                 </button>
-                        <span>{todo.name}</span>
-                        <input type="hidden" name="id" value={todo.id} />
-                        <button
-                            className="bg-red-500 px-2 py-1 rounded-lg text-sm text-white"
-                            formAction={deleteTodo}
-                            >
-                                削除
-                            </button>
                         </form>
                     </li>
                 ))}
