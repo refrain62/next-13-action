ee8d4d15e9ae18db92a33c4ab2fe4abfc35058c6
refrain62 <nao_h13@hotmail.com>
2023-06-13 22:54:40 +0900

    入力フォームの表示

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -11,6 +11,17 @@ const Page = async () => {
                     <li key={todo.id}>{todo.name}</li>
                 ))}
             </ul>
+
+            <form className="flex items-center mt-4">
+                <label htmlFor="name">Name:</label>
+                <input type="text" name="name" className="border mx-2 p-1" />
+                <button
+                    type="submit"
+                    className="bg-blue-600 px-2 py-1 rounded-lg text-sm text-white"
+                    >
+                        Add Todo
+                    </button>
+            </form>
         </div>
     );
 };
